c4519a0cd74e91372319a26b9b8b66ed497298f1
Esteban Velasquez <jestebanvelasquez22@gmail.com>
2022-06-29 19:50:39 -0500

    mejorando apariencia

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import bg from '../../assets/imgs/tarantino.png'
+import bg from '../../assets/bgOne.png'
 import './Header.css'
 
 export default function Header() {
   return (
     <section className='main-section'>
-      <h1 className='tittle'>Welcome To Tarantino Movies</h1>
-            
-                {/* <Link exact to='/home/create'>
-                    <button>To Create!</button>
-                </Link> */}
-                
-        </section>
+      <h1  >Welcome To Tarantino Movies</h1>
+      <img  src={bg} alt="" />
+    </section>
   )
 }
